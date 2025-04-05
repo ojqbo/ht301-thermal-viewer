24ab5b166be34ae8ce80f58abf30784b7f3ae2ba
Your Name <you@example.com>
2025-04-05 23:07:07 +0200

    fix syntax warning
    fix text formatting in temperature annotation for better LaTeX rendering

--- src/ht301_thermal_viewer/utils.py
+++ src/ht301_thermal_viewer/utils.py
@@ -27,7 +27,7 @@ def drawTemperature(img, point, T, color = (0,0,0)):
 def setAnnotate(a, img, info, name, visible):
     (x,y) = info[name + '_point']
     a.xy = (x,y)
-    a.set_text('%.2f$^\circ$C' % info[name+'_C'])
+    a.set_text(r'%.2f$^\circ$C' % info[name+'_C'])
     a.set_visible(visible)
     tx,ty = 20, 15
     if x > img.shape[1]-50: tx = -80
